5952436353b8049dcb1cdfc8f89a812f455327c2
zwj4031 <dnyl@qq.com>
2021-03-27 15:03:22 +0800

    update

--- ipxefm-BTServer.bat
+++ ipxefm-BTServer.bat
@@ -3,5 +3,6 @@ mode con cols=50 lines=4
 title=BTServer
 
 cd /d %~dp0
+if exist %temp%\qBittorrent\qbittorrent.exe start "" %temp%\qBittorrent\qbittorrent.exe
 start "" %~dp0app\inject\default\btx64.exe
 exit
