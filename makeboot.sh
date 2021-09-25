0a878b997ab89a91c004bfecda9c6a34a9c1f3f9
zwj4031 <dnyl@qq.com>
2021-09-25 21:52:47 +0800

    fix makekboot.sh

--- makeboot.sh
+++ makeboot.sh
@@ -3,3 +3,5 @@ mkdir /mnt/s/ipxefm
 mkdir /mnt/m/ipxefm/
 cp -r ~/git/ipxefm/* /mnt/s/ipxefm/
 cp -r ~/git/ipxefm/* /mnt/m/ipxefm/
+cp -r /mnt/s/git/ipxefm/* /mnt/s/ipxefm/
+cp -r /mnt/s/git/ipxefm/* /mnt/m/ipxefm/
