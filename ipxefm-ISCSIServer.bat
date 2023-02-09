c9f05939bd6ed562a456a8c0de95900b976badd3
zwj4031 <dnyl@qq.com>
2023-02-09 13:21:07 +0800

    add iscsiconsole

---
+++ ipxefm-ISCSIServer.bat
@@ -0,0 +1,22 @@
+@echo off
+cd /d %~dp0
+mode con cols=50 lines=4
+title=ISCSIServer
+if not "%SystemDrive%" == "C:" echo WinPE&&goto start 
+:: 获取管理员权限运行批处理
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if '%errorlevel%' NEQ '0' (
+goto UACPrompt
+) else ( goto gotAdmin )
+:UACPrompt
+echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+"%temp%\getadmin.vbs" 1>nul 2>nul
+exit /b
+:gotAdmin
+if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" ) 1>nul 2>nul
+
+
+cd /d %~dp0bin
+start "" %~dp0bin\iscsiconsole.EXE
+exit
