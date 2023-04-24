8d0d3176f1bdcace7871bdd9f621d2ff4fa8322f
zwj4031 <dnyl@qq.com>
2023-04-24 21:31:39 +0800

    add rem

--- ipxefm-GhostServer.bat
+++ ipxefm-GhostServer.bat
@@ -1,7 +1,13 @@
 @echo off
-mode con cols=50 lines=4
+mode con cols=50 lines=10
 title=GhostServer
+echo 自动确认一般都要加 -sure
+echo 克隆后重启添加 -rb
+echo gpt克隆报错参数添加 -ntexact
+echo linux克隆添加 -ib
 
 cd /d %~dp0bin
 start "" %~dp0bin\GhostSrv64.EXE
+echo 
+pause
 exit
